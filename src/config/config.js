c078b2ac91e074f2708a5a864b57c43b231433f4
Kayode Okunlade <kayodeokunlade@Kayodes-MacBook-Pro.local>
2019-03-25 19:04:20 +0100

    chore(database): Configure application to use Postgresql

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    use_env_variable: 'DATABASE_URL'
+  },
+  test: {
+    use_env_variable: 'TEST_DATABASE_URL'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL'
+  }
+};
