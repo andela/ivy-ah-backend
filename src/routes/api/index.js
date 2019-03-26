a5ebb15f717b20c97ec782fcd4c77a18b65ffaa0
Jesse Omoefe <jesseomoefe@Jesses-MacBook-Pro.local>
2019-03-26 18:14:21 +0100

    161780557-feat(documentation): API documentation
    - setup a base API documentation using Swagger
    
    [Delivers 161780557]

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../../documentation.json');
 
 router.use("/", require("./users"));
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 router.use(function(err, req, res, next) {
     if (err.name === "ValidationError") {
