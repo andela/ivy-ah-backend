fb804560f0410f40a078af91121ec40e8cba626b
jesse-efe <jesseefe@gmail.com>
2019-04-03 12:51:49 +0100

    161780562-feat(server): Implement social auth - user can signup with social media site [Delivers 161780562]

---
+++ src/config/socialAuth.js
@@ -0,0 +1,36 @@
+import passport from 'passport';
+import { Strategy as FacebookStrategy } from 'passport-facebook';
+import { OAuth2Strategy as googleStrategy } from 'passport-google-oauth';
+import { Strategy as twitterStrategy } from 'passport-twitter';
+import { socialAuth } from '../routes/api/users/auth';
+
+const socialCallback = async (accessToken, refreshToken, profile, done) => {
+  const email = profile.emails[0].value;
+  const username = profile.provider === 'twitter' ? profile.username : profile.name.givenName;
+  const userDetail = await socialAuth(email,
+    username);
+  return done(null, userDetail);
+};
+passport.serializeUser((userDetail, done) => done(null, userDetail));
+passport.use(new FacebookStrategy({
+  clientID: process.env.facebookClientID,
+  clientSecret: process.env.facebookClientSecret,
+  callbackURL: process.env.facebookCallbackUrl,
+  profileFields: ['id', 'email', 'displayName', 'name', 'photos'],
+},
+socialCallback));
+
+passport.use(new googleStrategy({
+  clientID: process.env.googleConsumerKey,
+  clientSecret: process.env.googleConsumerSecret,
+  callbackURL: process.env.googleCallbackUrl,
+},
+socialCallback));
+
+passport.use(new twitterStrategy({
+  consumerKey: process.env.twitterConsumerKey,
+  consumerSecret: process.env.twitterConsumerSecret,
+  callbackURL: process.env.twitterCallbackUrl,
+  includeEmail: true
+},
+socialCallback));
