bda59f7ece1ba1c9e9dee8e4617895506622d521
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2019-04-11 07:05:53 +0100

    161780565-feature(rating):rating articles
    - create endpoint to rate and update rating
    - create endpoint to get the total rating
    - add tests
    
    Finishes #161780565

--- src/routes/api/articles/articleRoutes.js
+++ src/routes/api/articles/articleRoutes.js
@@ -3,9 +3,13 @@ import validate from '../../../middlewares/validator/index';
 import authenticator from '../../../middlewares/authorization';
 import articleController from './articleController';
 import searchArticles from './searchArticles';
+import rating from './rateArticleController';
 
-const { validateArticle, validateArticleSearch } = validate;
 const { createArticle, getArticlesByPage } = articleController;
+const {
+  validateArticle, validateArticleSearch, validateArticleRating, validateGetArticleRating
+} = validate;
+const { rateArticleHandler, getRatings } = rating;
 
 const articleRoute = Router();
 
@@ -14,5 +18,7 @@ articleRoute.get('/search', validateArticleSearch, searchArticles);
 
 articleRoute.get('/', getArticlesByPage);
 
+articleRoute.post('/rating', authenticator, validateArticleRating, rateArticleHandler);
+articleRoute.get('/rating/:articleId', authenticator, validateGetArticleRating, getRatings);
 
 export default articleRoute;
